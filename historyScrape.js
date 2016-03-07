e7f009c891ce9e572f9e89b02874565bbaa01d74
Patrick Trasborg <patrick.trasborg@gmail.com>
2016-03-07 14:16:30 -0500

    data is successfully scraped, need to save it to files

--- historyScrape.js
+++ historyScrape.js
@@ -1,15 +1,24 @@
 var rp = require('request-promise');
 var cheerio = require('cheerio');
 
+const fs = require('fs');
+const path = require('path');
+
 var year = '2001';
 var resultArr = [3, 6, 9, 12, 15, 18];
+
+var fileDirectory = path(__dirname, '/models/');
 
 rp(`http://www.cbssports.com/collegebasketball/ncaa-tournament/history/yearbyyear/${year}`)
 .then(function(htmlString){
 	$ = cheerio.load(htmlString);
-	var data = $($('tr.row2').contents()[3]).text().replace(/\n/g, "!").replace(/!{2,}/g, '!').split('!');
+	var winnersLosersArr = resultArr.map(function(sliceNum, index){
+		var data = $($('tr.row2').contents()[sliceNum]).text().replace(/\n/g, "!").replace(/!{2,}/g, '!').split('!');
+		if(index === 0) return parseWinners(data, 3);
+		return parseWinners(data);
+	});
 
-	console.log('these are the results', parseWinners(data, 3));
+	console.log('these are the results', winnersLosersArr);
 })
 .catch(function(err){
 	console.log('there was an err', err);
@@ -25,4 +34,13 @@ function parseWinners(data, sliceNum){
 	    acc.losers.push(curr[1].replace(/No. \d{1,2} /, ""));
 	    return acc;
 	}, {winners: [], losers: []});
+}
+
+function promisifiedWriteFile(file, data){
+	return new Promise(function(resolve, reject){
+		fs.writeFile(file, data, function(err){
+			if(err) reject(err);
+			else resolve(file + " completed");
+		});
+	});
 }
